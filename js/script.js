47b5eff8c843111928d5b45fcee1c886f02546bd
Robert-Stefan <robertboss05@hotmail.it>
2021-04-10 13:35:27 +0200

    Work in progress near to finish

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    // References
+
+    var clickMenu = $(".with-dropdown")
+    var dropMenu = $(".drop-menu");
+
+    //FUNCTIONS TOGGLE
+    clickMenu.click(function(){
+        dropMenu.not($(this).find(".drop-menu")).hide();
+        $(this).find(".drop-menu").toggle();
+    })
+
+    //FUNCTIONS CLICK OUT
+    $("main").not($("header")).click(function(){
+        dropMenu.hide();
+    })
+
+})
